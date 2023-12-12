b482765ba3836bee24df843acff32c7f2598ad92
SmitSheth01 <smitsheth01@gmail.com>
2023-12-13 00:24:56 +0530

    Login/ Register UI

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,16 +1,26 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import {
+	Flex,
+	Input, Button, Checkbox, Form
+} from 'antd';
 import { Toaster } from 'react-hot-toast';
 import { loginUser } from '../../store/auth/authActions';
 import { errorToast } from '../../utils';
+import SecondaryLogo from '../../assets/logo/Secondary-logo.png';
+import Logo from '../../assets/logo/logo.PNG';
+import Email from '../../assets/logo/Email.png';
+import Password from '../../assets/logo/Password.png';
 
 export const Login = () => {
 	const { user, loading } = useSelector((state) => state.auth);
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
-	let email = useRef('');
-	let password = useRef('');
+	const email = useRef('');
+	const password = useRef('');
 
 	console.log('get user', user);
 	useEffect(() => {
@@ -25,13 +35,16 @@ export const Login = () => {
 		}
 	}, [navigate, user]);
 
-	const signinUser = async (e) => {
-		e.preventDefault();
+	// const onFinish = () => {
+	// 	console.log('Received values of form: ');
+	// };
+	const signinUser = async (values) => {
+		// e.preventDefault();
 
 		dispatch(
 			loginUser({
-				email: email.value,
-				password: password.value,
+				email: values.email,
+				password: values.password,
 			})
 		)
 			.unwrap()
@@ -39,61 +52,146 @@ export const Login = () => {
 				errorToast(errorData.error);
 			});
 	};
+	// const onFinish = (values) => {
+	// 	console.log('Received values of form: ', values);
+	// 	console.log('EMail: ', values.email);
+	// 	console.log('Password: ', values.password);
+	// };
 
 	return (
-		<section className='p-8'>
-			<Toaster />
-			<form onSubmit={signinUser}>
-				<div>
-					<h1>Sign In</h1>
-					<p className='mt-4 mb-8 text-lime-700'>
-            If you don't have an account <br />
-            You can{' '}
-						<Link to='/register/doctor' className='link'>
-              Register here !
-						</Link>
-					</p>
-				</div>
-				<div>
-					<label className='block text-primary-grey text-[13px] font-medium pb-1'>
-            Email
-					</label>
-					<div className='relative'>
-						<input
-							className='inputField w-full mb-8'
-							name='email'
-							placeholder='Enter your email'
-							id='email'
-							ref={(e) => {
-								email = e;
+		<div className='w-full flex h-screen'>
+			<div className='w-2/5 flex items-center justify-center gap-10'>
+				<div className='flex flex-col p-20 items-start '>
+					<div className='flex flex-col p-10 p-x-20 items-center gap-10 align-stretch'>
+						<img src={Logo} alt="Main Logo" className='back-image object-cover'/>
+					</div>
+					<div className="flex flex-col p-10 p-x-5 items-start gap-10">
+						<Form
+							name="normal_login"
+							className="login-form"
+							initialValues={{
+								remember: true,
 							}}
-							type='email'
-							required
-						/>
+							onFinish={signinUser}
+						>
+							<Form.Item
+								name="email"
+								rules={[
+									{
+										required: true,
+										message: 'Please input your Email Address!',
+									},
+									{
+										type: 'email'
+									}
+								]}
+							>
+								<Flex>
+									<Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email Address" />
+									<img src={Email} alt="Email Icon" className="w-7 h-7 self-center mr-2" />
+								</Flex>
+							</Form.Item>
+							<Form.Item
+								name="password"
+								rules={[
+									{
+										required: true,
+										message: 'Please input your Password!',
+									},
+								]}
+							>
+								<Flex>
+									<Input.Password
+										prefix={<LockOutlined className="site-form-item-icon" />}
+										type="password"
+										placeholder="Password"
+									/>
+									<img src = {Password} alt="Pasword Icon" className="w-7 h-7 self-center mr-2" />
+								</Flex>
+
+							</Form.Item>
+							<Form.Item>
+								<Form.Item name="remember" valuePropName="checked" noStyle>
+									<Checkbox>Remember me</Checkbox>
+								</Form.Item>
+
+								<a className="login-form-forgot" href="">
+          Forgot password
+								</a>
+							</Form.Item>
+
+							<Form.Item>
+								<Button type="primary" htmlType="submit" className="docButton">
+          Log in
+								</Button>
+        Or <a href="">register now!</a>
+							</Form.Item>
+						</Form>
 					</div>
 				</div>
-				<div>
-					<label className='block text-primary-grey text-[13px] font-medium pb-1'>
-            Password
-					</label>
-					<div className='relative'>
-						<input
-							className='inputField w-full mb-8'
-							name='password'
-							placeholder='Enter your password'
-							id='password'
-							ref={(e) => {
-								password = e;
-							}}
-							type='password'
-							required
-						/>
-					</div>
+			</div>
+			<div className='w-3/5 flex items-center justify-center' style = {{ backgroundColor: '#CCD6E5' }}>
+				<div className=''>
+					<img src={SecondaryLogo}
+						alt='Background Logo'
+						className='back-image object-cover h-full' />
 				</div>
-				<button type='submit' className='primaryButton mt-4'>
-					{loading ? 'Loading...' : 'Login'}
-				</button>
-			</form>
-		</section>
+
+			</div>
+		</div>
+		// <section className='p-8'>
+		// 	<Toaster />
+		// 	<form onSubmit={signinUser}>
+		// 		<div>
+		// 			<h1>Sign In</h1>
+		// 			<p className='mt-4 mb-8 text-lime-700'>
+	//     If you don't have an account <br />
+	//     You can{' '}
+		// 				<Link to='/register/doctor' className='link'>
+	//       Register here !
+		// 				</Link>
+		// 			</p>
+		// 		</div>
+		// 		<div>
+		// 			<label className='block text-primary-grey text-[13px] font-medium pb-1'>
+	//     Email
+		// 			</label>
+		// 			<div className='relative'>
+		// 				<input
+		// 					className='inputField w-full mb-8'
+		// 					name='email'
+		// 					placeholder='Enter your email'
+		// 					id='email'
+		// 					ref={(e) => {
+		// 						email = e;
+		// 					}}
+		// 					type='email'
+		// 					required
+		// 				/>
+		// 			</div>
+		// 		</div>
+		// 		<div>
+		// 			<label className='block text-primary-grey text-[13px] font-medium pb-1'>
+	//     Password
+		// 			</label>
+		// 			<div className='relative'>
+		// 				<input
+		// 					className='inputField w-full mb-8'
+		// 					name='password'
+		// 					placeholder='Enter your password'
+		// 					id='password'
+		// 					ref={(e) => {
+		// 						password = e;
+		// 					}}
+		// 					type='password'
+		// 					required
+		// 				/>
+		// 			</div>
+		// 		</div>
+		// 		<button type='submit' className='primaryButton mt-4'>
+		// 			{loading ? 'Loading...' : 'Login'}
+		// 		</button>
+		// 	</form>
+		// </section>
 	);
 };
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,35 +1,55 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-unused-vars */
 import { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import {
+	Flex,
+	Input, Button, Checkbox, Form, Row, Col
+} from 'antd';
 import { Toaster } from 'react-hot-toast';
 import { errorToast, successToast } from '../../utils';
+import SecondaryLogo from '../../assets/logo/Secondary-logo.png';
+import Logo from '../../assets/logo/logo.PNG';
+import Email from '../../assets/logo/Email.png';
 import { registerUser } from '../../store/auth/authActions';
 
 export const Register = () => {
 	const dispatch = useDispatch();
 	const { loading } = useSelector((state) => state.auth);
 	const [validationError, setValidationError] = useState('');
-	let fullName = useRef('');
-	let email = useRef('');
-	let password = useRef('');
-
+	const fullName = useRef('');
+	const email = useRef('');
+	const password = useRef('');
+	const [form] = Form.useForm();
+	const onFinish = (values) => {
+		console.log('Received values of form: ', values);
+	};
 	const signUpUser = (e) => {
-		e.preventDefault();
-		e.stopPropagation();
-
+		// e.preventDefault();
+		// e.stopPropagation();
 		// validation for all fields
 		if (
-			!fullName.value.trim()
-      || !email.value.trim()
-      || !password.value.trim()
+			!e.firstName.trim
+	  || !e.lastName.trim
+	  || !e.email.trim
 		) {
 			setValidationError('All fields are required');
 		} else {
 			dispatch(
 				registerUser({
-					fullName: fullName.value,
-					email: email.value,
-					password: password.value,
+					firstName: e.firstName,
+					lastName: e.lastName,
+					email: e.email,
+					password: e.password,
+					doctorId: e.doctor,
+					hospital_name: e.hospital,
+					address: e.address,
+					city: e.city,
+					pincode: e.pincode,
+					state: e.state,
+					country: e.country
 				})
 			)
 				.unwrap()
@@ -44,83 +64,259 @@ export const Register = () => {
 	};
 
 	return (
-		<section className='p-8'>
-			<Toaster />
-			<form onSubmit={signUpUser}>
-				<div>
-					<h1>Sign Up</h1>
-					<p className='mt-4 mb-8'>
-            If you already have an account registered <br />
-            You can{' '}
-						<Link to='/login' className='link'>
-              Login here !
-						</Link>
-					</p>
-				</div>
-				<div>
-					<label className='block text-primary-grey text-[13px] font-medium pb-1'>
-            Full Name
-					</label>
-					<div className='relative'>
-						<input
-							className='inputField mb-8 w-full'
-							name='fullName'
-							placeholder='Enter your full name'
-							id='fullName'
-							onChange={() => setValidationError('')}
-							ref={(e) => {
-								fullName = e;
+		<div className='w-full flex h-screen'>
+			<div className='w-2/5 flex items-center justify-center gap-10'>
+				<div className='flex flex-col p-20 items-start'>
+					<div className='flex flex-col p-10 p-x-20 items-center gap-10 align-stretch'>
+						<img src={Logo} alt="Main Logo" className='back-image object-cover'/>
+					</div>
+					<div className="flex flex-col p-10 p-x-5 items-start gap-10">
+						<Form
+							form={form}
+							layout="horizontal"
+							// labelCol={{ span: 6 }}
+							// wrapperCol={{ span: 16 }}
+							initialValues={{
+								remember: true,
 							}}
-							type='text'
-							required
-						/>
+							onFinish={signUpUser}
+						>
+							<Row gutter={24} className='flex px-4'>
+
+								<Form.Item
+
+									name="firstName" className='mx-1' rules={[
+										{
+											required: true,
+											message: 'Please input your First Name!',
+										}]}>
+
+									<Input placeholder='First Name' />
+
+								</Form.Item>
+
+								<Form.Item
+
+									name="lastName" className='mx-1' rules={[
+										{
+											required: true,
+											message: 'Please input your LastName!',
+										}]}>
+
+									<Input placeholder = 'Last Name' />
+
+								</Form.Item>
+
+							</Row>
+							<Row gutter={24}>
+								<Col span={24}>
+									<Form.Item name="email" rules={[
+										{
+											type: 'email'
+										},
+										{
+											required: true,
+											message: 'Please enter your Email Address!'
+										}
+									]} >
+										<Input placeholder = 'Email Address' />
+									</Form.Item>
+								</Col>
+							</Row>
+							<Row gutter={24}>
+								<Col span={24}>
+									<Form.Item name="doctor" rules={[
+										{
+											required: true,
+											message: 'Please input your Doctor ID!',
+										}]}>
+										<Input placeholder='Doctor ID ' />
+									</Form.Item>
+								</Col>
+							</Row>
+							<Row gutter={24}>
+								<Col span={24}>
+									<Form.Item name="password" rules={[
+										{
+											required: true,
+											message: 'Please enter your Password!',
+										}]}>
+										<Input.Password placeholder='Password'/>
+									</Form.Item>
+								</Col>
+							</Row>
+							<Row gutter={24}>
+								<Col span={24}>
+									<Form.Item name="confirmPassword" rules={[
+										{
+											required: true,
+											message: 'Re-enter password!',
+										}]}>
+										<Input.Password placeholder='Confirm Password'/>
+									</Form.Item>
+								</Col>
+							</Row>
+							<Row gutter={24}>
+								<Col span={24}>
+									<Form.Item name="hospital" rules={[
+										{
+											required: true,
+											message: 'Please enter your Hospital Name!',
+										}]}>
+										<Input placeholder='Hospital' />
+									</Form.Item>
+								</Col>
+							</Row>
+							<Row gutter={24}>
+								<Col span={24}>
+									<Form.Item name="address" rules={[
+										{
+											required: true,
+											message: 'Please enter your address!',
+										}]}>
+										<Input.TextArea placeholder='Enter your Address here'/>
+									</Form.Item>
+								</Col>
+							</Row>
+							<Row gutter={24}>
+								<Col span={12}>
+									<Form.Item name="city" rules={[
+										{
+											required: true,
+											message: 'Please enter your City!',
+										}]}>
+										<Input placeholder='City'/>
+									</Form.Item>
+								</Col>
+								<Col span={12}>
+									<Form.Item name="state" rules={[
+										{
+											required: true,
+											message: 'Please enter your State!',
+										}]}>
+										<Input placeholder='State'/>
+									</Form.Item>
+								</Col>
+							</Row>
+							<Row gutter={24}>
+								<Col span={12}>
+									<Form.Item name="pincode" rules={[
+										{
+											required: true,
+											message: 'Please enter your Pincode',
+										}]}>
+										<Input placeholder='Pincode' />
+									</Form.Item>
+								</Col>
+								<Col span={12}>
+									<Form.Item name="country" rules={[
+										{
+											required: true,
+											message: 'Please enter your country!',
+										}]}>
+										<Input placeholder='Country' />
+									</Form.Item>
+								</Col>
+							</Row>
+							<Row>
+								<Col span={24}>
+									<Form.Item>
+										<Button htmlType="submit" className='docButton'>
+              Register
+										</Button>
+            Or <a href="#">Login Now!</a>
+									</Form.Item>
+								</Col>
+							</Row>
+						</Form>
 					</div>
 				</div>
-				<div>
-					<label className='block text-primary-grey text-[13px] font-medium pb-1'>
-            Email
-					</label>
-					<div className='relative'>
-						<input
-							className='inputField mb-8 w-full'
-							name='email'
-							placeholder='Enter your email'
-							id='email'
-							onChange={() => setValidationError('')}
-							ref={(e) => {
-								email = e;
-							}}
-							type='email'
-							required
-						/>
-					</div>
+			</div>
+			<div className='w-3/5 flex items-center justify-center' style = {{ backgroundColor: '#CCD6E5' }}>
+				<div className=''>
+					<img src={SecondaryLogo}
+						alt='Background Logo'
+						className='back-image object-cover h-full' />
 				</div>
-				<div>
-					<label className='block text-primary-grey text-[13px] font-medium pb-1'>
-            Password
-					</label>
-					<div className='relative'>
-						<input
-							className='inputField mb-8 w-full'
-							name='password'
-							placeholder='Enter your password'
-							id='password'
-							onChange={() => setValidationError('')}
-							ref={(e) => {
-								password = e;
-							}}
-							type='password'
-							required
-						/>
-					</div>
-				</div>
-				{validationError && (
-					<p className='text-left text-red-500'>{validationError}</p>
-				)}
-				<button type='submit' className='primaryButton'>
-					{loading ? 'Loading...' : 'Register'}
-				</button>
-			</form>
-		</section>
+			</div>
+		</div>
+		// <section className='p-8'>
+		// 	<Toaster />
+		// 	<form onSubmit={signUpUser}>
+		// 		<div>
+		// 			<h1>Sign Up</h1>
+		// 			<p className='mt-4 mb-8'>
+	//     If you already have an account registered <br />
+	//     You can{' '}
+		// 				<Link to='/login' className='link'>
+	//       Login here !
+		// 				</Link>
+		// 			</p>
+		// 		</div>
+		// 		<div>
+		// 			<label className='block text-primary-grey text-[13px] font-medium pb-1'>
+	//     Full Name
+		// 			</label>
+		// 			<div className='relative'>
+		// 				<input
+		// 					className='inputField mb-8 w-full'
+		// 					name='fullName'
+		// 					placeholder='Enter your full name'
+		// 					id='fullName'
+		// 					onChange={() => setValidationError('')}
+		// 					ref={(e) => {
+		// 						fullName = e;
+		// 					}}
+		// 					type='text'
+		// 					required
+		// 				/>
+		// 			</div>
+		// 		</div>
+		// 		<div>
+		// 			<label className='block text-primary-grey text-[13px] font-medium pb-1'>
+	//     Email
+		// 			</label>
+		// 			<div className='relative'>
+		// 				<input
+		// 					className='inputField mb-8 w-full'
+		// 					name='email'
+		// 					placeholder='Enter your email'
+		// 					id='email'
+		// 					onChange={() => setValidationError('')}
+		// 					ref={(e) => {
+		// 						email = e;
+		// 					}}
+		// 					type='email'
+		// 					required
+		// 				/>
+		// 			</div>
+		// 		</div>
+		// 		<div>
+		// 			<label className='block text-primary-grey text-[13px] font-medium pb-1'>
+	//     Password
+		// 			</label>
+		// 			<div className='relative'>
+		// 				<input
+		// 					className='inputField mb-8 w-full'
+		// 					name='password'
+		// 					placeholder='Enter your password'
+		// 					id='password'
+		// 					onChange={() => setValidationError('')}
+		// 					ref={(e) => {
+		// 						password = e;
+		// 					}}
+		// 					type='password'
+		// 					required
+		// 				/>
+		// 			</div>
+		// 		</div>
+		// 		{validationError && (
+		// 			<p className='text-left text-red-500'>{validationError}</p>
+		// 		)}
+		// 		<button type='submit' className='primaryButton'>
+		// 			{loading ? 'Loading...' : 'Register'}
+		// 		</button>
+		// 	</form>
+		// </section>
 	);
 };
