96cc887dea9f8faa458f6b2bb8a4a318b2e13a57
krushit dudhat <krushitdudhat2001@gmail.com>
2023-12-31 16:46:27 +0530

    fix: static server deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import { App } from './components/app';
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<Provider store={store}>
-		<Router>
+		<Router basename='/src'>
 			{/* <React.StrictMode> */}
 			<App />
 			{/* </React.StrictMode> */}
