2810b6d502a7c658a2537afc915ed798d2c28300
krushit dudhat <krushitdudhat2001@gmail.com>
2023-12-31 17:15:09 +0530

    fix: bg image import

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
 		<div>
 			<Navbar />
 			<div className=''>
-				<div className='bg-[url("/src/assets/doctor/hero_bg.png")] bg-cover h-96 content-center flex items-center ps-10'>
+				<div className='bg-[url("../public/assets/hero_bg.png")] bg-cover h-96 content-center flex items-center ps-10'>
 					<div className='container m-auto'>
 						<div className='w-1/5 p-2 flex flex-col h-fit space-y-4 m-10'>
 							<h1 className='text-[#1A2F4E] font-[Inria_Serif]'> Labs </h1>
