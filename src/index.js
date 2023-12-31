966918337ccaa166b9030ccad2fbfd1da10875d3
krushit dudhat <krushitdudhat2001@gmail.com>
2023-12-31 16:49:01 +0530

    fix: base path

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<Provider store={store}>
-		<Router basename='/src'>
+		<Router basename='/'>
 			{/* <React.StrictMode> */}
 			<App />
 			{/* </React.StrictMode> */}
