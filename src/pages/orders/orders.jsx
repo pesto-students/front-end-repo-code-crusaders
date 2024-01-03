70da8a397264fc57fdf216e052d305b39d4df020
krushit dudhat <krushitdudhat2001@gmail.com>
2024-01-03 23:28:06 +0530

    add: doctor orders page

--- src/pages/orders/orders.jsx
+++ src/pages/orders/orders.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import {
-	Spin, Pagination, Card, Image,
+	Spin, Pagination, Card, Image, Steps
 } from 'antd';
+import { CurrencyRupee, CloseOutlined } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { Navbar } from '../../components/navbar';
-import { LabCard } from '../../components/labCard';
 import { getOrders } from '../../store/order/orderAction';
 
 const { Meta } = Card;
 
 const Orders = () => {
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
 	const {
 		orders, loading, error, pagination
-	} = useSelector((state) => state.lab);
+	} = useSelector((state) => state.order);
 	const [page, setPage] = React.useState(1);
 	const pageLimit = 20;
 
@@ -43,11 +45,15 @@ const Orders = () => {
 	return (
 		<div>
 			<Navbar />
-			<div className=''></div>
 			<div className='container m-auto'>
+				<div className='container m-auto my-2 w-3/4 flex justify-between align-middle'>
+					<h2> Orders </h2>
+					<CloseOutlined onClick={() => navigate(-1)} className='cursor-pointer' />
+				</div>
+				<hr className='w-3/4 container m-auto' />
 				<div className='flex px-10 flex-wrap'>
 					{error && 'No Content...'}
-					{orders && orders.map((order, index) => <OrderCard key={index} order={order} index={index} className='m-5'/>)}
+					{orders && orders.map((order, index) => <OrderCard key={index} order={order} index={index} className='my-2'/>)}
 				</div>
 				<Pagination
 					defaultCurrent={1}
@@ -63,13 +69,25 @@ const Orders = () => {
 	);
 };
 
-const OrderCard = ({ order }) => {
+const OrderCard = ({ order, className }) => {
 	const { product } = order;
+
+	const status = ['pending', 'accepted', 'readyToShip', 'outForDelivery', 'delivered'];
+	let currentStatusIndex = 0;
+	const timelineItems = status.map((state, index) => {
+		const item = {
+			title: state,
+		};
+		if (order.status === state) {
+			currentStatusIndex = index;
+		}
+		return item;
+	});
 
 	console.log(order);
 	return (
 		<div
-			className='w-2/3 m-auto cursor-pointer'
+			className={`w-2/3 m-auto cursor-pointer ${className}`}
 		>
 			<Card
 				className={'border-2 flex p-1 shadow-lg'}
@@ -77,7 +95,7 @@ const OrderCard = ({ order }) => {
 					<Image
 						alt='example'
 						src={product.images[0]}
-						width={250}
+						width={258}
 						preview={false}
 						className='w-400 rounded-b-md'
 					/>
@@ -86,17 +104,23 @@ const OrderCard = ({ order }) => {
 				<Meta title={product.name} />
 				<div className='flex my-2 text-lg'>{product?.details?.features}</div>
 				<div className='my-2 text-lg font-sans'>
-					{/* <CurrencyRupee /> <span> {product.price} </span> */}
+					<CurrencyRupee /> <span> {product.price} </span>
 				</div>
-				<div>
-					{/* <Rate
+				<div className='my-2'>
+					<Steps
+						progressDot
+						items={timelineItems}
+						current={currentStatusIndex}
+					/>
+				</div>
+				{/* <div>
+					<Rate
 						allowHalf={true}
 						defaultValue={product.rating}
-						disabled
 						className=''
-					/> */}
+					/>
 					<span> {parseFloat(product.rating).toFixed(1)} </span>
-				</div>
+				</div> */}
 			</Card>
 		</div>
 	);
