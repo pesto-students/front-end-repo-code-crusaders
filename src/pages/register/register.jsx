fac3d08878fba2f582ce2bdbde3f8cd0878f37d0
SmitSheth01 <smitsheth01@gmail.com>
2024-01-04 01:34:50 +0530

    Change registration page UI for doctor according to Lab

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import React from 'react';
+import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-	Input, Button, Form, Row, Col
+	Input, Button, Form, message
 } from 'antd';
+import { useNavigate } from 'react-router-dom';
 import { errorToast, successToast } from '../../utils';
 import SecondaryLogo from '../../assets/logo/bg_logo.png';
 import Logo from '../../assets/logo/nav_logo_black.png';
@@ -10,230 +12,320 @@ import { registerUser } from '../../store/auth/authActions';
 
 export const Register = () => {
 	const dispatch = useDispatch();
-	const { loading } = useSelector((state) => state.auth);
-	const [validationError, setValidationError] = useState('');
+	const { loading, error, success } = useSelector((state) => state.auth);
 	const [form] = Form.useForm();
+	const navigate = useNavigate();
 
-	// const onFinish = (values) => {
-	// 	console.log('Received values of form: ', values);
-	// };
-	console.log(validationError);
-	const signUpUser = (e) => {
-		// e.preventDefault();
-		// e.stopPropagation();
-		// validation for all fields
-		if (
-			!e.firstName.trim
-			|| !e.lastName.trim
-			|| !e.email.trim
-		) {
-			setValidationError('All fields are required');
-		} else {
-			const params = {
-				firstName: e.firstName,
-				lastName: e.lastName,
-				email: e.email,
-				password: e.password,
-				doctorId: e.doctor,
-				hospital_name: e.hospital,
-				address: e.address,
-				city: e.city,
-				pincode: e.pincode,
-				state: e.state,
-				country: e.country,
-			};
-			dispatch(
-				registerUser({
-					body: params,
-					role: 'doctor'
-				})
-			)
-				.unwrap()
-				.then(() => {
-					e.target.reset();
-					successToast('User Registered Successfully');
-				})
-				.catch((errorData) => {
-					errorToast(errorData.error);
-				});
+	const validatePassword = (_, value) => {
+		if (value.length < 8) {
+			return Promise.reject(new Error('Password must be at least 8 characters'));
 		}
+		if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+			return Promise.reject(new Error('Password must contain at least 1 letter and 1 number'));
+		}
+		return Promise.resolve();
+	};
+
+	React.useEffect(() => {
+		if (success) {
+			message.success('Doctor Registered Successfully');
+			navigate('/home');
+		} else if (error) {
+			message.error('Error Registering Doctor');
+		}
+	}, [error, success, navigate]);
+
+	const onFinish = (values) => {
+		console.log('form ', values);
+		const params = {
+			firstname: values.firstName,
+			lastname: values.lastName,
+			email: values.email,
+			password: values.password,
+			doctor: {
+				name: values.clinicName,
+				regID: values.doctorId,
+			},
+			address: {
+				address1: values.address1,
+				address2: values.address2,
+				city: values.city,
+				state: values.state,
+				country: values.country,
+				pincode: values.pincode
+			}
+		};
+
+		dispatch(registerUser({ body: params, role: 'doctor' }));
+	};
+
+	const onFinishFailed = (errorInfo) => {
+		message.error(`Form validation error!${errorInfo}`);
+		console.log('Failed:', errorInfo);
+
+		return false;
 	};
 
 	return (
 		<div className='w-full flex h-screen'>
-			<div className='w-2/5 flex items-center justify-center gap-10'>
-				<div className='flex flex-col p-20 items-start'>
-					<div className='flex flex-col p-10 p-x-20 items-center gap-10 align-stretch'>
-						<img src={Logo} alt="Main Logo" className='back-image object-cover'/>
+			<div className='w-2/5 flex items-center justify-center overflow-hidden bg-[#CCD6E5]'>
+				<div className=''>
+					<img
+						src={SecondaryLogo}
+						alt='Background Logo'
+						className='back-image object-cover h-full'
+					/>
+				</div>
+			</div>
+			<div className='w-3/5 flex items-center justify-center overflow-y-scroll'>
+				<div className='flex flex-col m-auto w-1/2'>
+					<div className='p-10'>
+						<img src={Logo} alt='Main Logo' className='' />
 					</div>
-					<div className="flex flex-col p-10 p-x-5 items-start gap-10">
+					<div className='flex flex-col p-10 p-x-5 items-start gap-10'>
 						<Form
 							form={form}
-							layout="horizontal"
 							name='doctorRegister'
+							// layout='horizontal'
 							// labelCol={{ span: 6 }}
 							// wrapperCol={{ span: 16 }}
 							initialValues={{
+								firstName: 'Smit',
+								lastName: 'Sheth',
+								email: `s+${Math.floor(Math.random() * 100)}@gmail.com`,
+								password: '1234dfewf',
+								confirm: '12343e2322',
+								address1: '23 A, john',
+								address2: 'nava faliya',
+								clinicName: 'dental Lab',
+								state: 'gujarat',
+								city: 'surat',
+								country: 'india',
+								pincode: '234598',
+								doctorId: '2335432523',
 								remember: true,
 							}}
-							onFinish={signUpUser}
+							onFinish={onFinish}
+							onFinishFailed={onFinishFailed}
+							className='w-full'
 						>
-							<Row gutter={24} className='flex px-4'>
-
+							<div className='flex justify-between'>
 								<Form.Item
-
-									name="firstName" className='mx-1' rules={[
+									name='firstName'
+									className='w-1/2 mx-1'
+									rules={[
 										{
 											required: true,
 											message: 'Please input your First Name!',
-										}]}>
-
+										},
+									]}
+								>
 									<Input placeholder='First Name' />
-
 								</Form.Item>
 
 								<Form.Item
-
-									name="lastName" className='mx-1' rules={[
+									name='lastName'
+									className='w-1/2 mx-1'
+									rules={[
 										{
 											required: true,
 											message: 'Please input your LastName!',
-										}]}>
-
-									<Input placeholder = 'Last Name' />
-
-								</Form.Item>
-
-							</Row>
-							<Row gutter={24}>
-								<Col span={24}>
-									<Form.Item name="email" rules={[
-										{
-											type: 'email'
 										},
-										{
-											required: true,
-											message: 'Please enter your Email Address!'
-										}
-									]} >
-										<Input placeholder = 'Email Address' />
-									</Form.Item>
-								</Col>
-							</Row>
-							<Row gutter={24}>
-								<Col span={24}>
-									<Form.Item name="doctor" rules={[
-										{
-											required: true,
-											message: 'Please input your Doctor ID!',
-										}]}>
-										<Input placeholder='Doctor ID ' />
-									</Form.Item>
-								</Col>
-							</Row>
-							<Row gutter={24}>
-								<Col span={24}>
-									<Form.Item name="password" rules={[
+									]}
+								>
+									<Input placeholder='Last Name' />
+								</Form.Item>
+							</div>
+							<Form.Item
+								name='email'
+								className='mx-1'
+								rules={[
+									{
+										type: 'email',
+										message: 'Please Enter valid Email Address',
+									},
+									{
+										required: true,
+										message: 'Please enter your Email Address!',
+									},
+								]}
+							>
+								<Input placeholder='Email Address' />
+							</Form.Item>
+
+							<div className='flex justify-between'>
+								<Form.Item
+									name='password'
+									className='w-1/2 mx-1'
+									rules={[
 										{
 											required: true,
 											message: 'Please enter your Password!',
-										}]}>
-										<Input.Password placeholder='Password'/>
-									</Form.Item>
-								</Col>
-							</Row>
-							<Row gutter={24}>
-								<Col span={24}>
-									<Form.Item name="confirmPassword" rules={[
+										},
+										{
+											validator: validatePassword,
+										}
+									]}
+									hasFeedback
+								>
+									<Input.Password placeholder='Password' />
+								</Form.Item>
+
+								<Form.Item
+									name='confirm'
+									className='w-1/2 mx-1'
+									dependencies={ ['password'] }
+									hasFeedback
+									rules={[
 										{
 											required: true,
-											message: 'Re-enter password!',
-										}]}>
-										<Input.Password placeholder='Confirm Password'/>
-									</Form.Item>
-								</Col>
-							</Row>
-							<Row gutter={24}>
-								<Col span={24}>
-									<Form.Item name="hospital" rules={[
+											message: 'Please confirm your password!',
+										},
+										({ getFieldValue }) => ({
+											validator(_, value) {
+												if (!value || getFieldValue('password') === value) {
+													return Promise.resolve();
+												}
+												return Promise.reject(new Error('The new password that you entered do not match!'));
+											},
+										}),
+									]}
+								>
+									<Input.Password placeholder='Confirm Password' />
+								</Form.Item>
+							</div>
+
+							<div className='flex justify-between'>
+								<Form.Item
+									name='doctorId'
+									className=' w-1/2 mx-1'
+									rules={[
 										{
 											required: true,
-											message: 'Please enter your Hospital Name!',
-										}]}>
-										<Input placeholder='Hospital' />
-									</Form.Item>
-								</Col>
-							</Row>
-							<Row gutter={24}>
-								<Col span={24}>
-									<Form.Item name="address" rules={[
+											message: 'Please enter your Doctor ID!'
+										}
+									]}
+								>
+									<Input placeholder='Doctor Id' />
+								</Form.Item>
+
+								<Form.Item
+									name='clinicName'
+									className='w-1/2 mx-1'
+									rules={[
 										{
 											required: true,
-											message: 'Please enter your address!',
-										}]}>
-										<Input.TextArea placeholder='Enter your Address here'/>
-									</Form.Item>
-								</Col>
-							</Row>
-							<Row gutter={24}>
-								<Col span={12}>
-									<Form.Item name="city" rules={[
+											message: 'Please input your Clinic Name!',
+										},
+									]}
+								>
+									<Input placeholder='Dental Clinic Name' />
+								</Form.Item>
+							</div>
+
+							<Form.Item
+								name='address1'
+								className=' mx-1'
+								rules={[
+									{
+										required: true,
+										message: 'Please enter your address!',
+									},
+								]}
+							>
+								<Input placeholder='House No. / street name / block' />
+							</Form.Item>
+							<Form.Item
+								name='address2'
+								className=' mx-1'
+								rules={[
+									{
+										required: true,
+										message: 'Please enter your address!',
+									},
+								]}
+							>
+								<Input placeholder='Area, landmark' />
+							</Form.Item>
+
+							<div className='flex justify-between'>
+								<Form.Item
+									name='city'
+									className='w-1/2 mx-1'
+									rules={[
 										{
 											required: true,
 											message: 'Please enter your City!',
-										}]}>
-										<Input placeholder='City'/>
-									</Form.Item>
-								</Col>
-								<Col span={12}>
-									<Form.Item name="state" rules={[
+										},
+									]}
+								>
+									<Input placeholder='City' />
+								</Form.Item>
+
+								<Form.Item
+									name='state'
+									className='w-1/2 mx-1'
+									rules={[
 										{
 											required: true,
 											message: 'Please enter your State!',
-										}]}>
-										<Input placeholder='State'/>
-									</Form.Item>
-								</Col>
-							</Row>
-							<Row gutter={24}>
-								<Col span={12}>
-									<Form.Item name="pincode" rules={[
+										},
+									]}
+								>
+									<Input placeholder='State' />
+								</Form.Item>
+							</div>
+
+							<div className='flex justify-between'>
+								<Form.Item
+									name='pincode'
+									className='w-1/2 mx-1'
+									rules={[
 										{
 											required: true,
 											message: 'Please enter your Pincode',
-										}]}>
-										<Input placeholder='Pincode' />
-									</Form.Item>
-								</Col>
-								<Col span={12}>
-									<Form.Item name="country" rules={[
+										},
+									]}
+								>
+									<Input placeholder='Pincode' />
+								</Form.Item>
+
+								<Form.Item
+									name='country'
+									className='w-1/2 mx-1'
+									rules={[
 										{
 											required: true,
 											message: 'Please enter your country!',
-										}]}>
-										<Input placeholder='Country' />
-									</Form.Item>
-								</Col>
-							</Row>
-							<Row>
-								<Col span={24}>
-									<Form.Item>
-										<Button htmlType="submit" className='docButton' loading={loading}>
-              Register
-										</Button>
-            Or <a href="/login">Login Now!</a>
-									</Form.Item>
-								</Col>
-							</Row>
+										},
+									]}
+								>
+									<Input placeholder='Country' />
+								</Form.Item>
+							</div>
+
+							<Form.Item className='w-full'>
+								<div className='flex flex-col space-y-3 w-full items-center'>
+									<Button
+										type='primary'
+										htmlType='submit'
+										className='docButton w-1/2'
+										loading={loading}
+									>
+				Doctor Sign up
+									</Button>
+									<div className='flex w-full'>
+										<hr className='w-1/2 bg-gray-200 border-0 dark:bg-gray-700' />
+										<span className='font-semibold'> OR </span>
+										<hr className='w-1/2 bg-gray-200 border-0 dark:bg-gray-700' />
+									</div>
+									<Button className='docButton w-1/2' onClick={() => navigate('/login', { state: { role: 'doctor' } })}>
+				Log in
+									</Button>
+								</div>
+							</Form.Item>
 						</Form>
 					</div>
-				</div>
-			</div>
-			<div className='w-3/5 flex items-center justify-center' style = {{ backgroundColor: '#CCD6E5' }}>
-				<div className=''>
-					<img src={SecondaryLogo}
-						alt='Background Logo'
-						className='back-image object-cover h-full' />
 				</div>
 			</div>
 		</div>
